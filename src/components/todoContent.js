17034fa09e39c85dc77020d7b6a850c191a10257
GagaPoloJr <rahmadiihaikal@gmail.com>
2021-12-29 20:52:34 +0700

    fix something

--- src/components/todoContent.js
+++ src/components/todoContent.js
@@ -15,7 +15,7 @@ const TodoContent = (props) => {
                         </p>
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                        <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Edit</button>
+                        <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">...</button>
                     </div>
                 </div>
             </li>
